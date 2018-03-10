728e053c56817ecb0d35de1f49b8622699cdb743
lorenzo-lipparini <lorenzo.lipparini.it@gmail.com>
2018-03-10 22:23:26 +0100

    Parallel animations now possible with KochSnowflake

--- koch-snowflake/KochCurve.js
+++ koch-snowflake/KochCurve.js
@@ -15,6 +15,7 @@ export default class KochCurve extends Animatable {
     // 0-iterations Koch curves are just straight lines, so they have no child curves
     if (this.iterations !== 0) {
       this.childCurves = [];
+      // No need to call _updateCurves here, since it will be called by the tanAngle setter
 
       // Used in the animation process to make the bumps come out of the line gradually
       this.tanAngle = 1.7320508075688767; // tan(60°)
@@ -34,24 +35,43 @@ export default class KochCurve extends Animatable {
 
     this._tanAngle = value;
 
-    this.childCurves = [];
-    this._createChildCurves();
-
+    this._updateChildCurves();
   }
 
-  _createChildCurves() {
+
+  _updatePosition(start, end) {
+    this.start = start;
+    this.end = end;
+
+    if (this.iterations !== 0) {
+      this._updateChildCurves();
+    }
+  }
+
+  // Creates the child curves or updates their position if they exist already
+  // Reusing the old children instead of replacing them is important because
+  // there might be animations bound to those objects, which would stop if they
+  // were destroyed
+  _updateChildCurves() {
 
     let previousX = this.start.x;
     let previousY = this.start.y;
 
-    // Helper function to create the child curves, considers the positions as
+    let addedCurves = 0;
+
+    // Helper function to create/update the child curves, considers the positions as
     // relative to this.start and works basically like beginShape()
     let addCurve = (relativeEndX, relativeEndY) => {
-      this.childCurves.push(new KochCurve(
-        new p5.Vector(previousX, previousY),
-        new p5.Vector((previousX = this.start.x + relativeEndX), (previousY = this.start.y + relativeEndY)),
-        this.iterations - 1
-      ));
+      let start = new p5.Vector(previousX, previousY);
+      let end = new p5.Vector((previousX = this.start.x + relativeEndX), (previousY = this.start.y + relativeEndY));
+      
+      if (this.childCurves[addedCurves] === undefined) {
+        this.childCurves.push(new KochCurve(start, end, this.iterations - 1));
+      } else {
+        this.childCurves[addedCurves]._updatePosition(start, end);
+      }
+
+      addedCurves++; 
     };
 
     let deltaX = this.end.x - this.start.x;
@@ -115,10 +135,10 @@ export default class KochCurve extends Animatable {
 
     // Simple case: this wasn't even a fractal
     if (this.iterations === 1) {
-      this.tanAngle = 1.7320508075688767; // tan(60°)
-
       this.childCurves = [];
-      this._createChildCurves();
+      // No need to call _updateCurves here, since it will be called by the tanAngle setter
+
+      this.tanAngle = 1.7320508075688767; // tan(60°)
 
       return;
     }
