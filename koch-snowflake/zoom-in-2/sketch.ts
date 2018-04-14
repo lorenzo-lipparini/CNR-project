bd0770c669d8e50e6ac6357910ae4fa03e088a32
lorenzo-lipparini <lorenzo.lipparini.it@gmail.com>
2018-04-14 22:07:10 +0200

    New variation of the zoom-in animation in koch-snowflake

--- koch-snowflake/zoom-in-2/sketch.ts
+++ koch-snowflake/zoom-in-2/sketch.ts
@@ -25,24 +25,34 @@ async function main() {
   const bumpUp = new LinearAnimation<KochCurve, 'tanAngle'>('tanAngle', 3, 0, Math.tan(Math.PI / 3));
 
   let childCurve = kochCurve;
+  let targetAngle = 0;
 
-  await animate(drawOptions, new LinearAnimation<typeof drawOptions, 'zoomPos'>('zoomPos', 4, kochCurve.start).toHarmonic());
-  await timer(0.5);
-
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 8; i++) {
     childCurve.incrementIterations();
     childCurve.animateIteration(childCurve.iterations, bumpUp);
 
-    childCurve = childCurve.childCurves[0];
+    childCurve = childCurve.childCurves[1];
 
-    await animate(drawOptions, new LinearAnimation<typeof drawOptions, 'zoomFactor'>('zoomFactor', 3, 3 * drawOptions.zoomFactor).toHarmonic());
+    const zoomCenter = childCurve.start.map((x, i) => (x + childCurve.end[i]) / 2);
+    
+    const previousAngle = targetAngle;
+    targetAngle = Math.atan2(childCurve.end[1] - childCurve.start[1], childCurve.end[0] - childCurve.start[0]);
+    // Make sure that the number of full turns in the previousAngle and the targetAngle match
+    targetAngle += (2*Math.PI) * Math.ceil((previousAngle - targetAngle) / (2*Math.PI));
+    
+    const zoomAnimation = new LinearAnimation<typeof drawOptions, 'zoomPos'>('zoomPos', 3, zoomCenter).toHarmonic()
+                .parallel(new LinearAnimation<typeof drawOptions, 'zoomFactor'>('zoomFactor', 3, 3 * drawOptions.zoomFactor).toHarmonic())
+                .parallel(new LinearAnimation<typeof drawOptions, 'angle'>('angle', 3, targetAngle).toHarmonic());
+
+    await animate(drawOptions, zoomAnimation);
   }
 
 }
 
 const drawOptions = {
   zoomPos: [0, 0, 0],
-  zoomFactor: 1
+  zoomFactor: 1,
+  angle: 0
 };
 
 window.draw = () => {
@@ -56,6 +66,7 @@ window.draw = () => {
   scale(1, -1);
 
   scale(drawOptions.zoomFactor);
+  rotate(-drawOptions.angle);
   translate(-drawOptions.zoomPos[0], -drawOptions.zoomPos[1]);
 
   noFill();
