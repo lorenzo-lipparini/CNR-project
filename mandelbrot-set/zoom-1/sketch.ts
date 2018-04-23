a180efdc611db6ee5ba20a746eecd8b31ef66a02
lorenzo-lipparini <lorenzo.lipparini.it@gmail.com>
2018-04-23 17:36:36 +0200

    First zoom animation in mandelbrot-set

---
+++ mandelbrot-set/zoom-1/sketch.ts
@@ -0,0 +1,33 @@
+
+import videoSpecs from '../../lib/videoSpecs.js';
+import FrameCapture from '../../lib/FrameCapture.js';
+import { LinearAnimation, animate, updateAnimations } from '../../lib/animation.js';
+
+import Mandelbrot from '../Mandelbrot.js';
+
+
+let mandelbrot: Mandelbrot;
+
+window.setup = () => {
+  createCanvas(videoSpecs.resolution.x, videoSpecs.resolution.y, WEBGL);
+  
+  mandelbrot = new Mandelbrot([-0.7463, 0.1102], 1/4, 500);
+
+  FrameCapture.acquire();
+
+  main();
+};
+
+async function main() {
+
+  await animate(mandelbrot, new LinearAnimation<Mandelbrot, 'zoomFactor'>('zoomFactor', 20, 50).toHarmonic());
+
+  FrameCapture.stop();
+}
+
+window.draw = () => {
+  updateAnimations();
+  mandelbrot.render();
+
+  FrameCapture.update();
+}
