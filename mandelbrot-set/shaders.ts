354868092dff02f79f42f4985e39b6e6423c890c
lorenzo-lipparini <lorenzo.lipparini.it@gmail.com>
2018-05-07 00:48:41 +0200

    New lerp pattern shaders in mandelbrot-set

--- mandelbrot-set/shaders.ts
+++ mandelbrot-set/shaders.ts
@@ -42,6 +42,31 @@ const makeFragSrc = (setColor: string) => `
 
 `;
 
+
+/**
+ * Ensures that all the numbers in the expression are written with the decimal point,
+ * so that glsl doesn't interpret them as integers instead of floating point values.
+ */
+function fix(strings: TemplateStringsArray, ...values: (number | string)[]): string {
+  let result = strings[0];
+
+  for (let i = 0; i < values.length; i++) {
+    const value = values[i];
+
+    if (typeof value === 'number') {
+      // If the value is a number, always show it with three digits, even if they are just zeros
+      result += value.toFixed(3);
+    } else {
+      // Do nothing to any other kind of data
+      result += value;
+    }
+    result += strings[i + 1];
+  }
+
+  return result;
+}
+
+
 /**
  * Creates a shader source which renders the mandelbrot set using only two colors;
  * The colors must be given as three comma-separated floating point values ranging from 0.0 to 1.0 representing the RGB components of the color.
@@ -52,6 +77,47 @@ const makeFragSrc = (setColor: string) => `
 const makeBinaryPattern = (inColor: string, outColor: string) => makeFragSrc(`
   gl_FragColor = (steps == -1) ? vec4(${inColor}, 1.0) : vec4(${outColor}, 1.0);
 `);
+
+/**
+ * Creates a shader source which renders the Mandelbrot set interpolating between a set of colors.
+ * The colors must be given as arrays of three numbers in the range [0, 1] representing the RGB components of the color.
+ * 
+ * @param inColor Color of the points inside the Mandebrot set
+ * @param outColorSteps Fixed color values used to linearly interpolate the color of the points outside the Mandelbrot set
+ */
+function makeLerpPattern(inColor: [number, number, number], outColorSteps: [number, number, number][]) {
+  let outColorExpr = '';
+
+  // The change in percent iterations between points colored with consecutive f colors
+  const interval = 1 / (outColorSteps.length - 1);
+
+  for (let i = 1; i < outColorSteps.length; i++) { // Foreach pair of consecutive step colors
+    const initialColor = outColorSteps[i - 1];
+    const finalColor = outColorSteps[i];
+
+    let gradientExpr = 'vec4(';
+    for (let j = 0; j < 3; j++) { // Foreach color component
+      // Write the code for a linear interpolation based on the number of steps
+      gradientExpr += fix`${initialColor[j]} + (percent - ${((i - 1) * interval)}) / ${interval} * (${finalColor[j] - initialColor[j]}), `;
+    }
+    gradientExpr += '1.0)';
+
+    if (i === outColorSteps.length - 1) { // If this is the last step
+      // Complete the previous ternary expression without opening a new one
+      outColorExpr += ` : ${gradientExpr}`;
+    } else {
+      // Build a chain of ternary expressions
+      outColorExpr += fix` : (percent <= ${i * interval}) ? ${gradientExpr}`;
+    }
+  }
+  
+  return makeFragSrc(fix`
+    float percent = float(steps - 50 * (steps / 50)) / 50.0;
+
+    gl_FragColor = (steps == -1) ? vec4(${inColor[0]}, ${inColor[1]}, ${inColor[2]}, 1.0)${outColorExpr};
+  `);
+}
+
 
 export const fragSrcs = {
 
@@ -82,6 +148,14 @@ export const fragSrcs = {
       1.0
     );
 
-  `)
+  `),
+
+  'ugly': makeLerpPattern([0, 0, 0], [
+    [0.000, 0.278, 4.667],
+    [0.639, 0.000, 0.000],
+    [1.000, 4.667, 0.000],
+    [0.937, 0.825, 0.553],
+    [0.000, 0.686, 0.710]
+  ])
 
 };
