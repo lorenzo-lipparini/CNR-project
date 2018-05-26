ec37f91fd57b1d74267764ea4849a280251d2cf4
lorenzo-lipparini <lorenzo.lipparini.it@gmail.com>
2018-05-26 13:54:59 +0200

    Now using the Line class to draw the grid in Plane2D

--- plane-2D/Plane2D.ts
+++ plane-2D/Plane2D.ts
@@ -1,4 +1,7 @@
 
+import Line from './Line.js';
+
+
 /**
  * Namespace containing functions that take care of the transformation of coordinates
  * from the 2D plane to the displayed canvas, as well as the visual representation of the plane itself. 
@@ -7,6 +10,11 @@ namespace Plane2D {
 
   let unitLength = 0;
 
+  let gridLines: {
+    horizontals: Line[],
+    verticals: Line[]
+  };
+
   /**
    * Used to correctly map points on the plane to points on the canvas;
    * To be called in draw() after setting the background.
@@ -14,10 +22,41 @@ namespace Plane2D {
    * @param value Distance in pixels between two points which are 1 unit apart on the plane
    */
   export function setUnitLength(value: number): void {
+    const needToUpdateGridLines = value !== unitLength;
+    
     translate(width/2, height/2);
     scale(value, -value);
 
     unitLength = value;
+
+    if (needToUpdateGridLines) {
+      updateGridLines();
+    }
+  }
+
+  /**
+   * Creates new Line objects that make the grid.
+   */
+  function updateGridLines(): void {
+    // Get rid of the previous lines
+    gridLines = {
+      horizontals: [],
+      verticals: []
+    };
+
+    // Maximum coordinates which fit on the screen
+    const maxX = width/2 / unitLength;
+    const maxY = height/2 / unitLength;
+
+    Line.defaultStyle = { rgb: [255, 255, 255], alpha: 100, strokeWeight: 0.01 };
+
+    // Foreach integer value n visible on the axes
+    for (let n = 1; n <= max(maxX, maxY); n++) {
+      gridLines.horizontals.push(new Line(-maxX, n, maxX, n));
+      gridLines.horizontals.push(new Line(-maxX, -n, maxX, -n));
+      gridLines.horizontals.push(new Line(n, -maxY, n, maxY));
+      gridLines.horizontals.push(new Line(-n, -maxY, -n, maxY));
+    }
   }
 
   /**
@@ -50,15 +89,11 @@ namespace Plane2D {
       line(-tickLength/2, -n, tickLength/2, -n);
       line(n, -tickLength/2, n, tickLength/2);
       line(-n, -tickLength/2, -n, tickLength/2);
+    }
 
-      if (showGrid) {
-        strokeWeight(0.005);
-        stroke(255, 255, 255, 100);
-
-        line(-maxX, n, maxX, n);
-        line(-maxX, -n, maxX, -n);
-        line(n, -maxY, n, maxY);
-        line(-n, -maxY, -n, maxY);
+    if (showGrid) {
+      for (const line of gridLines.horizontals.concat(gridLines.verticals)) {
+        line.show();
       }
     }
 
