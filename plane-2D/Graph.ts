3ff8778ff441b13fd267ed24f12fdedff2eb981a
lorenzo-lipparini <lorenzo.lipparini.it@gmail.com>
2018-06-09 18:43:23 +0200

    New drawFrom() animation function in Graph

--- plane-2D/Graph.ts
+++ plane-2D/Graph.ts
@@ -1,11 +1,13 @@
 
+import { Animatable, HarmonicAnimation } from '../lib/animation.js';
+
 import Plane2D from './Plane2D.js';
 
 
 /**
  * Class that makes it easy to draw the graph of a function, to be used in combination with Plane2D.
  */
-export default class Graph {
+export default class Graph extends Animatable {
 
   public interval: [number, number];
 
@@ -23,6 +25,8 @@ export default class Graph {
    */
   public constructor(f: (x: number) => number, plane: Plane2D, step?: number);
   public constructor(private f: (x: number) => number, intervalOrPlane: [number, number] | Plane2D, private step = 0.05) {
+    super();
+
     if (intervalOrPlane instanceof Plane2D) {
       const { minX, maxX } = intervalOrPlane.minMaxValues;
       this.interval = [minX, maxX];
@@ -31,7 +35,23 @@ export default class Graph {
     }
   }
 
+  /**
+   * Plays an animation where the curve is gradually drawn on the plane.
+   * 
+   * @param origin Point on the interval where the curve starts to be drawn
+   */
+  public drawFrom(origin: 'left' | 'right' | 'center'): Promise<void> {
+    const initialInterval: [number, number] =
+      (origin === 'left')  ? [this.interval[0], this.interval[0]] :
+      (origin === 'right') ? [this.interval[1], this.interval[1]] :
+      [(this.interval[0] + this.interval[1]) / 2, (this.interval[0] + this.interval[1]) / 2];
+
+    return this.animate(new HarmonicAnimation<Graph, 'interval'>('interval', 2, initialInterval, this.interval));
+  }
+
   public show(): void {
+    this.updateAnimations();
+
     noFill();
     strokeWeight(0.01);
     stroke(255, 255, 255);
