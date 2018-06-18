7bfbfa7554d15c80343d9b2746590943f3014934
lorenzo-lipparini <lorenzo.lipparini.it@gmail.com>
2018-06-19 01:59:05 +0200

    New flash and fade-out variants of menger-sponge/increasing-iterations

--- menger-sponge/increasing-iterations/sketch.ts
+++ menger-sponge/increasing-iterations/sketch.ts
@@ -1,44 +1,44 @@
 
 import videoSpecs from '../../lib/videoSpecs.js';
 import FrameCapture from '../../lib/FrameCapture.js';
-import timer from '../../lib/timer.js';
 import { PropertyAnimation, LinearAnimation, Animation } from '../../lib/animation.js';
+import timer from '../../lib/timer.js';
+import Scene from '../../lib/Scene.js';
 
 import Cube from '../Cube.js';
 import MengerSponge from '../MengerSponge.js';
 
 
-const angularVelocity = 1/30 * (2 * Math.PI / 60);
+const scene = new Scene();
 
-let mengerSponge: MengerSponge;
-
-
-window.setup = () => {
+window.setup = async () => {
   createCanvas(videoSpecs.resolution.x, videoSpecs.resolution.y, WEBGL);
 
-  mengerSponge = new MengerSponge([0, 0, 0], height / 2, [50, 100, 255], 0);
-
-  main();
-
   FrameCapture.acquire();
+
+  await main();
+
+  FrameCapture.stop();
 };
 
 async function main() {
+  const mengerSponge = new MengerSponge([0, 0, 0], height / 2, [50, 100, 255], 0);
+  scene.add(mengerSponge);
 
   mengerSponge.showExcludedCubes = true;
 
   const scaleDown = new LinearAnimation<Cube, 'side'>('side', 1.5, 0);
 
-  const flashColor = lerpColor(color(mengerSponge.color), color('white'), 0.2);
-  const flashColorV = [red(flashColor), green(flashColor), blue(flashColor)];
+  const flashColor = [90, 130, 255];
   const flash = new PropertyAnimation<Cube, 'color'>('color', 3, (progress, initialColor) =>
-    progress <= 0.25 ? flashColorV  :
+    progress <= 0.25 ? flashColor   :
     progress <= 0.50 ? initialColor :
-    progress <= 0.75 ? flashColorV  :
+    progress <= 0.75 ? flashColor   :
                        initialColor
   );
 
-  const cubeAnimation = flash.concat(scaleDown);
+  const fadeFactor = 2;
+  const fadeOut = new LinearAnimation<MengerSponge, 'color'>('color', 1.5, color => color.map(x => x / fadeFactor));
 
 
   await timer(1);
@@ -46,17 +46,20 @@ async function main() {
   while (mengerSponge.iterations < 3) {
     mengerSponge.incrementIterations();
 
-    await mengerSponge.animateExcludedCubes(mengerSponge.iterations, cubeAnimation);
+    // (flash variant)
+    // await mengerSponge.animateExcludedCubes(mengerSponge.iterations, flash);
+    // (fade-out variant)
+    // mengerSponge.animate(fadeOut);
+    await mengerSponge.animateExcludedCubes(mengerSponge.iterations, scaleDown);
 
     await timer(1/3);
   }
 
-
-  FrameCapture.stop();
 }
 
 
 let angle = 0;
+const angularVelocity = 1/30 * (2 * Math.PI / 60);
 
 window.draw = () => {
   timer.update();
@@ -73,7 +76,7 @@ window.draw = () => {
 
   noStroke();
   
-  mengerSponge.show();
+  scene.render();
 
 
   FrameCapture.update();
